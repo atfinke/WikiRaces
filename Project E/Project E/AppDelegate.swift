d8bebb0ad12a9fe290ae90efd9eaa5c69d0bd3c5
Andrew Finke <andrewfinke@me.com>
2020-03-24 20:22:08 -0500

    cleaning

--- Project E/Project E/AppDelegate.swift
+++ Project E/Project E/AppDelegate.swift
@@ -14,20 +14,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: WKRUIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+
         window = WKRUIWindow(frame: UIScreen.main.bounds)
         let controller = UIViewController()
         let nav = WKRUINavigationController(rootViewController: controller)
         nav.setNavigationBarHidden(true, animated: false)
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
-        
+
         return true
     }
 
-
 }
-
