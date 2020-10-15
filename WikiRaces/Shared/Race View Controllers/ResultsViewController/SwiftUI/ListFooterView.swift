0d07a420a0b7fc55341cfb519cbde64d2de391b5
Andrew Finke <andrewfinke@me.com>
2020-10-14 20:06:17 -0500

    interface adjustments

--- WikiRaces/Shared/Race View Controllers/ResultsViewController/SwiftUI/ListFooterView.swift
+++ WikiRaces/Shared/Race View Controllers/ResultsViewController/SwiftUI/ListFooterView.swift
@@ -9,34 +9,36 @@ import SwiftUI
 import WKRUIKit
 
 struct ListFooterView: View {
-
+    
     @Environment(\.colorScheme) var colorScheme: ColorScheme
-
+    
     let topText: String
     let bottomText: String
     let textOpacity: Double
-
+    
     var body: some View {
-        VStack {
-            Divider().frame(height: 2)
-                .padding(.bottom, 10)
-            Text(topText)
-                .font(.system(size: 14, weight: .medium))
-                .foregroundColor(.wkrSubtitleTextColor(for: colorScheme))
-                .opacity(textOpacity)
-                .animation(nil, value: topText)
-                .animation(.easeInOut(duration: 0.4), value: textOpacity)
-            Spacer()
-            Text(bottomText)
-                .font(Font(UIFont(monospaceSize: 16, weight: .medium)))
-                .foregroundColor(.wkrTextColor(for: colorScheme))
-                .opacity(textOpacity)
-                .animation(nil, value: bottomText)
-                .animation(.easeInOut(duration: 0.4), value: textOpacity)
-
-        }
-        .frame(minHeight: 55, maxHeight: 55)
-        .padding(.bottom, 20)
+        GeometryReader { geometry in
+            VStack {
+                Divider().frame(height: 2)
+                Spacer()
+                Text(topText)
+                    .font(.system(size: 14, weight: .medium))
+                    .foregroundColor(.wkrSubtitleTextColor(for: colorScheme))
+                    .opacity(textOpacity)
+                    .animation(nil, value: topText)
+                    .animation(.easeInOut(duration: 0.4), value: textOpacity)
+                Spacer().frame(height: 8)
+                Text(bottomText)
+                    .font(Font(UIFont(monospaceSize: 16, weight: .medium)))
+                    .foregroundColor(.wkrTextColor(for: colorScheme))
+                    .opacity(textOpacity)
+                    .animation(nil, value: bottomText)
+                    .animation(.easeInOut(duration: 0.4), value: textOpacity)
+                Spacer()
+                
+            }
+            .offset(x: 0, y: geometry.safeAreaInsets.bottom / 2)
+        }.frame(height: 75)
     }
-
+    
 }
