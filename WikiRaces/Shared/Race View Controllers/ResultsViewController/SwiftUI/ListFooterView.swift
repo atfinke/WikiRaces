c924b3e3adbf4079ae9486445b5a82dc961e4673
Andrew Finke <andrewfinke@me.com>
2020-10-16 12:42:00 -0500

    interface enhancements

--- WikiRaces/Shared/Race View Controllers/ResultsViewController/SwiftUI/ListFooterView.swift
+++ WikiRaces/Shared/Race View Controllers/ResultsViewController/SwiftUI/ListFooterView.swift
@@ -17,27 +17,23 @@ struct ListFooterView: View {
     let textOpacity: Double
     
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                Divider().frame(height: 2)
-                Spacer()
-                Text(topText)
-                    .font(.system(size: 14, weight: .medium))
-                    .foregroundColor(.wkrSubtitleTextColor(for: colorScheme))
-                    .opacity(textOpacity)
-                    .animation(nil, value: topText)
-                    .animation(.easeInOut(duration: 0.4), value: textOpacity)
-                Spacer().frame(height: 8)
-                Text(bottomText)
-                    .font(Font(UIFont(monospaceSize: 16, weight: .medium)))
-                    .foregroundColor(.wkrTextColor(for: colorScheme))
-                    .opacity(textOpacity)
-                    .animation(nil, value: bottomText)
-                    .animation(.easeInOut(duration: 0.4), value: textOpacity)
-                Spacer()
-                
-            }
-            .offset(x: 0, y: geometry.safeAreaInsets.bottom / 2)
+        VStack {
+            Divider().frame(height: 2)
+            Spacer()
+            Text(topText)
+                .font(.system(size: 14, weight: .medium))
+                .foregroundColor(.wkrSubtitleTextColor(for: colorScheme))
+                .opacity(textOpacity)
+                .animation(nil, value: topText)
+                .animation(.easeInOut(duration: 0.4), value: textOpacity)
+            Spacer().frame(height: 8)
+            Text(bottomText)
+                .font(Font(UIFont(monospaceSize: 16, weight: .medium)))
+                .foregroundColor(.wkrTextColor(for: colorScheme))
+                .opacity(textOpacity)
+                .animation(nil, value: bottomText)
+                .animation(.easeInOut(duration: 0.4), value: textOpacity)
+            Spacer()
         }.frame(height: 75)
     }
     
